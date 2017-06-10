4def7d8c75251a7663a5068510a96b152b292ae2
bagvendt <mgmano@gmail.com>
2017-06-10 12:07:10 +0200

    Work on MyDate. Range Functions an Iterable

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,18 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+fun upperBoundedDaysSinceJesusOurLordAndSaviourWasBord(date: MyDate) : Int {
+    return date.dayOfMonth -1 + (date.month -1 ) * 31 + date.year * 365
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return upperBoundedDaysSinceJesusOurLordAndSaviourWasBord(this) - upperBoundedDaysSinceJesusOurLordAndSaviourWasBord(other)
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(start = this, endInclusive = other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +20,16 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate>{
+    operator fun contains(date : MyDate) = start <= date && date <= endInclusive
+    override fun iterator(): Iterator<MyDate> = object : Iterator<MyDate> {
+        var it : MyDate = start
+        override fun hasNext(): Boolean = it <= endInclusive
+        override fun next(): MyDate {
+            val current = it
+            it = it.nextDay()
+            return current
+        }
+
+    }
+}
